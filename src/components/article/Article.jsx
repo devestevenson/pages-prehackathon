196bd84be8a8089bea3da43ce0d64a86879da64e
dev estevenson <107928127+devestevenson@users.noreply.github.com>
2022-11-05 15:31:42 -0500

    Ajustes en noticias

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,45 +1,35 @@
-import React, {useEffect, useState} from 'react'
-import "../article/Article.css"
-import Imagen from "../article/imagen.jpg"
+import React, { useEffect, useState } from "react";
+import "../article/Article.css";
+import Imagen from "../article/imagen.jpg";
 
 const Article = () => {
-    const [nosotros, setNosotros] = useState([])
+  const [nosotros, setNosotros] = useState([]);
 
-    const getNosotros = async () => {
-        let response = await fetch("https://pre-hackathon-dvag.herokuapp.com/api/articles")
-        let data = await response.json()
-       
-        setNosotros(data.articles)
-    }
-    useEffect(() => {
-        getNosotros();
-      }, []);
+  const getNosotros = async () => {
+    let response = await fetch(
+      "https://pre-hackathon-dvag.herokuapp.com/api/articles"
+    );
+    let data = await response.json();
 
-
+    setNosotros(data.articles);
+  };
+  useEffect(() => {
+    getNosotros();
+  }, []);
 
   return (
-    <div className='container-Article'>
-      {
-      nosotros.map(nosotro =>(
-      <div className='text'>
-        <h2>{nosotro.title}</h2>
-        <p>{nosotro.description}</p>
-      </div>
-    ))
-  }
-      {/* <div className="text">
-
-        <h2>Conoce nuestra historia</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos sapiente fugit placeat voluptas error et, sed accusamus ratione minima aperiam sint excepturi, repellat omnis adipisci! Magnam quas adipisci quia asperiores.
-
-        Consequuntur itaque necessitatibus qui. Eum quam animi delectus eveniet, fugit explicabo, nobis facilis corrupti molestias, quos nemo laudantium consequuntur consequatur. Minima doloribus, natus nobis nam nihil dolor mollitia obcaecati autem.
-       </p>    
-      </div> */}
+    <div className="container-Article">
+      {nosotros.map((nosotro) => (
+        <div className="text">
+          <h2>{nosotro.title}</h2>
+          <p>{nosotro.description}</p>
+        </div>
+      ))}
       <div className="imagen">
         <img src={Imagen} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
--- src/components/news/Noticias.jsx
+++ src/components/news/Noticias.jsx
@@ -1,39 +1,45 @@
-import React, {useEffect,useState} from 'react'
-import "../news/Noticias.css"
+import React, { useEffect, useState } from "react";
+import "../news/Noticias.css";
 
 const Noticias = () => {
-    const [noticias, setNoticias] = useState([])
+  const [noticias, setNoticias] = useState([]);
 
-    const getNoticias = async () => {
-        let response = await fetch("url")
-        let data = await response.json()
-        setNoticias(data)
-    }
-    useEffect(() => {
-        getNoticias();
-      }, []);
-
-
-
-
+  const getNoticias = async () => {
+    let response = await fetch("url");
+    let data = await response.json();
+    setNoticias(data);
+  };
+  useEffect(() => {
+    getNoticias();
+  }, []);
 
   return (
-    <div className='container-Noticia'>
-        <div className="container-tarjeta">
-            <div className="image-noticia">
-             <img src="https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
-            </div>
-            <h3>titulo</h3>
-            <p className='fecha'>05-20-22</p>
-            <p className='autor'>Pipito torres</p>
-            <p className='descripcion'> grupo feliz</p>
+    <div className="container-Noticia">
+      <div className="container-tarjeta">
+        <div className="image-noticia">
+          <img
+            src="https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt=""
+          />
         </div>
-      
 
-
-
+        <div className="container-Article">
+          {/* {noticias.map((noticia) => (
+            <div className="text">
+              <h3>{noticia.title}</h3>
+              <p className="fecha">{noticia.fecha}</p>
+              <p className="autor">{noticia.autor}</p>
+              <p className="description">{noticia.description}</p>
+            </div>
+          ))} */}
+        </div>
+        <h3>titulo</h3>
+        <p className="fecha">05-20-22</p>
+        <p className="autor">Pipito torres</p>
+        <p className="descripcion"> grupo feliz</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Noticias
+export default Noticias;
